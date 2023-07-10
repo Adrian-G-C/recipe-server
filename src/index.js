b3b554035130bffb9b693247975088ff64e1f595
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-10 18:28:11 -0500

    databse connected

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ const app = express();
 app.use (express.json());
 app.use (cors());
 
+mongoose.connect(
+    "mongodb+srv://aciocan:N1BwzS5m6xBTftXK@project3-recipeapp.ysmq71b.mongodb.net/project3-recipeapp?retryWrites=true&w=majority"
+    )
+
 app.listen(3002, () => console.log('Server Working!'));
