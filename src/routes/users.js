3a3e27477b861edec9e68d1b7476f532300c6336
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-10 19:10:47 -0500

    user registration complete

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,24 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import { UserModel } from '../models/Users.js'
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    const { username, passwrod } = req.body;
+    const { username, password } = req.body;
     const user = await UserModel.findOne({ username });
-    res.json(user);
+
+    if (user) {
+        return res.json({message: "User already exists!"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = new UserModel({username, password: hashedPassword});
+    await newUser.save();
+
+    res.json({ message: "User Has Been Registered!"});
 });
 
 router.post("/login");
