e88a184e80ea960d7ac7e51b0245607fe5d4efdb
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-11 15:08:26 -0500

    get request for recipies complete

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 import { userRouter } from './routes/users.js';
+import { recipesRouter } from './routes/recipes.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use (express.json());
 app.use (cors());
 
 app.use('/auth', userRouter);
+app.use('/recipes', recipesRouter);
 
 mongoose.connect(
     "mongodb+srv://aciocan:N1BwzS5m6xBTftXK@project3-recipeapp.ysmq71b.mongodb.net/project3-recipeapp?retryWrites=true&w=majority",
