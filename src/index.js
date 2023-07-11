927b89d6b60aa9572b813776a4f94717e2d7e345
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-11 17:34:54 -0500

    middleware complete and cleaned up code structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-
-import { userRouter } from './routes/users.js';
-import { recipesRouter } from './routes/recipes.js';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
 
 const app = express();
 
-app.use (express.json());
-app.use (cors());
+app.use(express.json());
+app.use(cors());
 
-app.use('/auth', userRouter);
-app.use('/recipes', recipesRouter);
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
 
 mongoose.connect(
     "mongodb+srv://aciocan:N1BwzS5m6xBTftXK@project3-recipeapp.ysmq71b.mongodb.net/project3-recipeapp?retryWrites=true&w=majority",
