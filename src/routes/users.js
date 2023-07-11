ccb9cc3a629b5d0336791d5470cb546f46ac0d0e
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-10 19:23:12 -0500

    user login verification working

--- src/routes/users.js
+++ src/routes/users.js
@@ -14,13 +14,28 @@ router.post("/register", async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10)
-
     const newUser = new UserModel({username, password: hashedPassword});
     await newUser.save();
 
     res.json({ message: "User Has Been Registered!"});
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+        return res.json({ message: "User Does Not Exist!"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.json({ message: "Username or Password Is Incorrect!"});
+    }
+
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id });
+});
 
 export { router as userRouter };
