e068722b59cd2a7d95cc5435ff843b1510cd93cb
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-07-11 21:42:31 -0500

    test

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import cors from "cors";
 import { userRouter } from "./routes/user.js";
 import { recipesRouter } from "./routes/recipes.js";
 
+const corsOptions = {
+    origin: 'https://project3-recipeapp-front-edcfb839000c.herokuapp.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
